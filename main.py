a0e89481952c75cd88cb609f17ccce382a69cc1c
Dipec001 <dpecchukwu@gmail.com>
2023-08-05 11:53:02 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+def text_to_morse(any_text):
+    morse_code_dict = {
+        'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+        'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+        'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+        'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+        'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+        'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--',
+        '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
+        '9': '----.', ' ': '/'
+    }
+
+    morse_code = []
+    for char in any_text.upper():
+        morse_char = morse_code_dict.get(char)
+        if morse_char:
+            morse_code.append(morse_char)
+
+    return ' '.join(morse_code)
+
+
+# Example usage:
+text = input('What text would you like to change to morse?')
+morse_code = text_to_morse(text)
+print(morse_code)
